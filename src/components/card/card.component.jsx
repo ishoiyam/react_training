3283c10f797b4ba37a8da59ad6aeace5a14ad021
HackerIsho <hackerisho@localhost.localdomain>
2021-02-19 05:07:28 +0100

    adding searchField

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -8,5 +8,7 @@ export const Card  = (props) => (
             src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
         />
         <h1> {props.monster.name} </h1>
+        <p> {props.monster.email} </p>
+
     </div>
 )
